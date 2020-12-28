2cf73c8f57c816b0f2af9f459236cd6612305256
dongasai <1514582970@qq.com>
2020-12-28 23:07:22 +0800

    1

--- coverage.php
+++ coverage.php
@@ -7,6 +7,11 @@ use SebastianBergmann\CodeCoverage\Filter;
 use SebastianBergmann\CodeCoverage\Driver\Selector;
 use SebastianBergmann\CodeCoverage\CodeCoverage;
 use SebastianBergmann\CodeCoverage\Report\Html\Facade as HtmlReport;
+use SebastianBergmann\CodeCoverage\Report\Clover as CloverReport;
+use SebastianBergmann\CodeCoverage\Report\Cobertura as CoberturaReport;
+use SebastianBergmann\CodeCoverage\Report\Crap4j as Crap4jReport;
+use SebastianBergmann\CodeCoverage\Report\PHP as PHPReport;
+use SebastianBergmann\CodeCoverage\Report\Text as TextReport;
 
 $filter = new Filter;
 $filter->includeDirectory(__DIR__ . DIRECTORY_SEPARATOR . 'tester');
@@ -24,3 +29,8 @@ $coverage->stop();
 
 
 (new HtmlReport)->process($coverage, __DIR__ . DIRECTORY_SEPARATOR . '.tmp/'. date('Ymd'));
+(new CloverReport)->process($coverage, __DIR__ . DIRECTORY_SEPARATOR . '.tmp/'. date('Ymd_1'));
+(new CoberturaReport)->process($coverage, __DIR__ . DIRECTORY_SEPARATOR . '.tmp/'. date('Ymd_2'));
+(new Crap4jReport)->process($coverage, __DIR__ . DIRECTORY_SEPARATOR . '.tmp/'. date('Ymd_3'));
+(new TextReport)->process($coverage, __DIR__ . DIRECTORY_SEPARATOR . '.tmp/'. date('Ymd_4'));
+(new PHPReport)->process($coverage, __DIR__ . DIRECTORY_SEPARATOR .  date('Ymd_5').'.php');
